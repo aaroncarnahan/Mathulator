fceb8b951934004e976ee58692bb39d43d08b64f
Aaron Carnahan <aaron.b.carnahan@outlook.com>
2021-01-21 19:32:46 -0500

    added database

--- Mathulator/User.cs
+++ Mathulator/User.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Mathulator
 {
 	public class User
 	{
+		[Key]
 		public string UserName { get; set; }
 		public string UserPassword { get; set; }
 
